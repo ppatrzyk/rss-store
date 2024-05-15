b8a6c5d072cfa2a7ed3d3dbd83032de467236584
Piotr Patrzyk <piotr@patrzyk.me>
2024-05-15 11:39:24 +0200

    use asyncpg; save actual rss

--- flows/feed.py
+++ flows/feed.py
@@ -1,23 +1,24 @@
 import asyncio
+import asyncpg
+from datetime import datetime
 import feedparser
 import os
-
 from prefect import flow, task
-
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import text
+import time
 
 db_conn_str = os.environ["RSS_DB"]
-engine = create_async_engine(db_conn_str)
 
 @task
 async def get_feed_list():
     """
     Get feed URLs from db
     """
-    async with engine.connect() as conn:
-        result = await conn.execute(text("select id, url from source"))
-    feeds = tuple(result.mappings())
+    feeds = []
+    conn = await asyncpg.connect(db_conn_str)
+    async with conn.transaction():
+        async for r in conn.cursor('select id, url from source'):
+            feeds.append({"id": r[0], "url": r[1]})
+    await conn.close()
     return feeds
 
 @task(retries=4)
@@ -25,38 +26,42 @@ async def query_feed(feed):
     """
     Get new entries for a feed
     """
-    # TODO get feed
     source_id = feed.get("id")
     url = feed.get("url")
-    entries = tuple(f"{source_id}-entry-{i}" for i in range(5))
+    response = feedparser.parse(url)
     print(f"querying {source_id}")
-    print(f"entries {entries}")
-    return source_id, entries
+    return source_id, response
 
 @task(retries=4)
-async def save_entries(source_id, entries):
+async def save_entries(source_id, response):
     """
     Write entries to db
     """
-    # TODO batch write all entries
     query = """
         insert into
         content(source_id, publication_date, url, external_id, title, content)
-        values (:source_id, :publication_date, :url, :external_id, :title, :content)
+        values ($1, $2, $3, $4, $5, $6)
     """
-    params = {
-        "source_id": source_id,
-        "publication_date": "2024-05-01T20:20:20+00:00",
-        "url": "todo",
-        "external_id": "todo",
-        "title": "todo",
-        "content": entries[0]
-    }
-    async with engine.connect() as conn:
-        _result = await conn.execute(text(query), params)
-        # _commit = await conn.commit()
-    print("saving entries")
-    print(entries)
+    entries = []
+    for entry in response.entries:
+        publication_date = None
+        try:
+            publication_date = datetime.fromtimestamp(time.mktime(entry.published_parsed))
+        except:
+            pass
+        row = (
+            source_id,
+            publication_date,
+            entry.link,
+            entry.id,
+            entry.title,
+            entry.summary
+        )
+        entries.append(row)
+    conn = await asyncpg.connect(db_conn_str)
+    _result = await conn.executemany(query, entries)
+    await conn.close()
+    print(f"saving {len(entries)} entries for {source_id}")
     return True
 
 @flow(log_prints=True)
@@ -64,8 +69,8 @@ async def update(feed):
     """
     Flow for updating single feed
     """
-    source_id, entries = await query_feed(feed)
-    _save = await save_entries(source_id, entries)
+    source_id, response = await query_feed(feed)
+    _save = await save_entries(source_id, response)
     return True
 
 @flow(log_prints=True)
